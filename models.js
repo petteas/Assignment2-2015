7ad6cf44ab3d3f3822bd7d4383ca42115a2c8a1a
Sindre Magnussen <sindrma@gmail.com>
2015-04-23 10:40:38 -0700

    Initial commit.

--- models.js
+++ models.js
@@ -5,7 +5,7 @@ var findOrCreate = require('mongoose-findorcreate');
 var userSchema = mongoose.Schema({
 	"name" : { type: String },
   "ig_id" : { type: String},
-	"ig_access_token" : { type: String },
+	"ig_access_token" : { type: String }
 });
 
 exports.User = mongoose.model('User', userSchema);
