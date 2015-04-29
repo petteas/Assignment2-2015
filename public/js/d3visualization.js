3137b9b3f25be7a25282e0d597b2cc9c4ec83b31
Petter Astrup <petter.astrup@gmail.com>
2015-04-28 19:48:38 -0700

    Added number of post on feed er day-chart with dummy-data

--- public/js/d3visualization.js
+++ public/js/d3visualization.js
@@ -1,94 +1,62 @@
-var margin = {top: 20, right: 20, bottom: 100, left: 40};
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
-//define scale of x to be from 0 to width of SVG, with .1 padding in between
-var scaleX = d3.scale.ordinal()
-  .rangeRoundBands([0, width], .1);
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-//define scale of y to be from the height of SVG to 0
-var scaleY = d3.scale.linear()
-  .range([height, 0]);
+var parseDate = d3.time.format("%d-%b-%y").parse;
 
-//define axes
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
 var xAxis = d3.svg.axis()
-  .scale(scaleX)
-  .orient("bottom");
+    .scale(x)
+    .orient("bottom");
 
 var yAxis = d3.svg.axis()
-  .scale(scaleY)
-  .orient("left");
+    .scale(y)
+    .orient("left");
 
-var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(d) {
-      return "<strong>Count:</strong> <span style='color:red'>" + d.counts.media + "</span>";
-    })
+var line = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
 
-function drawProfileinfo(d) {
-  d3.select("body").select("#usernm")
-      .text("Username: "+d.username);
-  d3.select("body").select("#bio")
-      .text("Bio: "+ d.bio);
-  d3.select("body").select("#profpic")
-      .attr("src", d.profile_picture);
-}
-//create svg
 var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .style("margin-left","auto")
     .style("margin-right","auto")
     .style("display","block")
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.call(tip);
-//get json object which contains media counts
 d3.json('/igMediaCounts', function(error, data) {
-  //set domain of x to be all the usernames contained in the data
-  scaleX.domain(data.users.map(function(d) { return d.username; }));
-  //set domain of y to be from 0 to the maximum media count returned
-  scaleY.domain([0, d3.max(data.users, function(d) { return d.counts.media; })]);
 
-  //set up x axis
+var data = [{date:parseDate("1-May-12"),close:"582.13"},{date:parseDate("30-Apr-12"),close:"583.98"},{date:parseDate("27-Apr-12"),close:"603.00"},{date:parseDate("26-Apr-12"),close:"607.70"},{date:parseDate("25-Apr-12"),close:"610.00"}];
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.close; }));
+
   svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")") //move x-axis to the bottom
-    .call(xAxis)
-    .selectAll("text")  
-    .style("text-anchor", "end")
-    .attr("dx", "-.8em")
-    .attr("dy", ".15em")
-    .attr("transform", function(d) {
-      return "rotate(-65)" 
-    });
-console.log(data);
-  //set up y axis
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
   svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Number of Photos");
-  //set up bars in bar graph
-  svg.selectAll(".bar")
-    .data(data.users)
-    .enter().append("rect")
-      .on("click", function(d,i) {drawProfileinfo(d)})
-      .on('mouseover',tip.show)
-      .on('mouseout', tip.hide)
-    .attr("class", "bar")
-    .attr("x", function(d) { return scaleX(d.username); })
-    .attr("width", scaleX.rangeBand())
-    .attr("y", function(d) { return scaleY(d.counts.media); })
-    .attr("height", function(d) { return height - scaleY(d.counts.media); });
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Number of posts on your feed");
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
   d3.select("#loader")
       .remove();
-  d3.select("#userInfo")
-      .style("display", "inherit");
   d3.selectAll("svg").style("background","white");
 });
